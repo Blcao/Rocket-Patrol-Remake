0134e5b13b907efa2cbc455b8657bcaefb6219b0
Blcao <blcao@ucsc.edu>
2021-04-13 03:22:03 -0700

    added in a point system and menu, but bugged

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,8 +2,57 @@ class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
     }
-    create() {
-        this.add.text(20,20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+
+    preload() {
+        // load audio
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
+
+    create() {
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+
+            
+        }
+
+        //menu text
+        this.add.text(game.config.wdith/2, (game.config.height/2) - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.wdith/2, (game.config.height/2) - borderUISize, 'Use LEFT AND RIGHT TO MOVE & (F) to fire', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(game.config.wdith/2, (game.config.height/2) + borderUISize + borderPadding, 'Press LEFT for Novice and RIGHT for Expert', menuConfig).setOrigin(0.5);
+    }
+
+   /* update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // easy mode
+          game.settings = {
+            spaceshipSpeed: 3,
+            gameTimer: 60000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('playScene');    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // hard mode
+          game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('playScene');    
+        }
+      }
+      */
 }
